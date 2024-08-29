d841673ddf570cb106dac0e89c12edbe08439c06
João Fernando <jfcbraulio@gmail.com>
2024-08-29 10:44:43 -0300

    Login

---
+++ php/login.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nome = $_POST['username'];
+    $senha = $_POST['password'];
+
+    if($nome == "Joao" && $senha == "12345"){
+        setcookie('nome', $nome);
+        header("Location:./conta.php");
+    }else{
+        echo "Login ou senha incorretos. <br>";
+        echo "<a href=\"../pages/login.html\">Volte para o login</a>";
+    }
+
+?>
